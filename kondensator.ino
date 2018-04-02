1ea5236d806e2ab9b67b9e8e0e342fc3611511a0
Karosemi <37125422+Karosemi@users.noreply.github.com>
2018-04-02 17:42:19 +0200

    Add files via upload

---
+++ kondensator.ino
@@ -0,0 +1,41 @@
+#include<math.h>
+void setup() {
+  pinMode(A0,INPUT);
+  pinMode(12,OUTPUT);
+  Serial.begin(9600);}
+
+//digitalWrite(11,LOW);//rozladowywanie
+void loop() {
+  int Alfa=100;
+  int Beta=900;
+  int Cpin=12;
+  int Rpin=13;
+  int a;
+  unsigned long time_0=millis();
+  while(a<=Beta){
+    a=analogRead(A0);
+   digitalWrite(12,HIGH);
+    Serial.println(a);
+    delay(50);
+    }
+  unsigned long time_1=millis();  
+  while(a>=Alfa){
+  digitalWrite(12,LOW);
+    a = analogRead(A0);
+    Serial.println(a);
+    delay(50);
+    }
+  unsigned long time_2=millis();
+  unsigned long ChargeTime=time_1-time_0;
+  unsigned long DchargeTime=time_2-time_1;
+  
+  
+  long R=67;
+  long u=Beta/Alfa;
+  long C1=(DchargeTime)/R*1/log (u);
+  //Serial.println(C1);
+  long v=(1-Alfa/1023)/(1-Beta/1023);
+  long C2=(ChargeTime)/R*1/log (v);
+ //Serial.println(C2);
+  
+}
---
+++ sygnalizacja1.ino
@@ -0,0 +1,117 @@
+int red=11;
+int green=10;
+int blue=9;
+int redsyg=3;
+int yellowsyg=5;
+int greensyg=6;
+int a=0; //sygnal z przycisku
+int i=0;//licznik czasu dla sygnalizacji 
+int j=0;//licznik czasu dla diody RGB
+void setup() {
+  pinMode(2, INPUT);
+  pinMode(redsyg, OUTPUT);
+  pinMode(greensyg, OUTPUT);
+  pinMode(yellowsyg, OUTPUT);
+  pinMode(red, OUTPUT);
+  pinMode(green, OUTPUT);
+  pinMode(blue, OUTPUT);
+  pinMode(2, INPUT);
+}
+//funkcje kolorów dla diody RGB
+//czerwony
+void Red(){
+    digitalWrite(red,HIGH);
+    digitalWrite(green,LOW);
+    digitalWrite(blue,LOW);
+    }
+//zolty
+void Yellow(){
+    digitalWrite(red,HIGH);
+    digitalWrite(green,HIGH);
+    digitalWrite(blue,LOW);
+    }
+//zielony
+  void Green(){
+    digitalWrite(red,LOW);
+    digitalWrite(green,HIGH);
+    digitalWrite(blue,LOW);
+    }
+//cyjan
+ void Cyan(){
+    digitalWrite(red,LOW);
+    digitalWrite(green,HIGH);
+    digitalWrite(blue,HIGH);
+    }
+//niebieski
+void Blue(){
+    digitalWrite(red,LOW);
+    digitalWrite(green,LOW);
+    digitalWrite(blue,HIGH);
+    }
+//magenta
+void Magenta(){
+    digitalWrite(red,HIGH);
+    digitalWrite(green,LOW);
+    digitalWrite(blue,HIGH);
+ }
+ //bialy
+ void White(){
+    digitalWrite(red,HIGH);
+    digitalWrite(green,HIGH);
+    digitalWrite(blue,HIGH);
+ }   
+void loop() {
+  a=digitalRead(2); //zczytuje sygnal z przycisku i przypisuje mu zmienna a
+  //jezeli j<14 sekund to wysylany jest sygnal do diody czerwonej
+  if (i<14){
+    digitalWrite(redsyg,HIGH);
+    }
+  else{digitalWrite(redsyg,LOW);}//w innych przypadkach dioda gasnie
+  // sygnał dla diody zoltej
+  if((i>=12 and i<14)or(i>28 and i<=30)){
+    digitalWrite(yellowsyg,HIGH);}
+  else{digitalWrite(yellowsyg,LOW);}
+  //sygnal dla diody zielonej
+  if(i>=14 and i<=28){
+    digitalWrite(greensyg,HIGH);
+    /*jezeli zostanie nacisniety przycisk to a=1. Jezeli swieci sie wtedy dioda zielona
+    w ciagu pieciu sekund sygnalizacja zmienia kolor na czerwony (w tym czasie dioda RGB swieci swiatlem bialym*/
+    if(a==1){
+      White();//dioda RGB swieci swiatlem bialym
+      delay(3000);
+      digitalWrite(greensyg,LOW);//gaśnie zielone światlo
+      digitalWrite(yellowsyg,HIGH);//zapala sie żólte swiatlo
+      delay(2000);
+      i=0; //nastepnie sygnalizaca wraca do fazy poczatkowej
+      j=0;}}
+  else{digitalWrite(greensyg,LOW);}
+//Kiedy minie 30s licznik wraca do stanu poczatkowego
+  if (i==30){
+    i=0;}
+ //licznik dla diody RGB
+  if(j<2){
+    Red();} //przez pierwsze 2 sekundy dioda RGB swieci na czerwono
+  if(j>=2 and j<4){
+    Yellow();}//przez kolejne 2 sekundy dioda RGB swieci na żolto
+  if(j>=4 and j<6){
+    Green();//przez kolejne 2 sekundy dioda RGB swieci na zielono
+  }
+  if(j>=6 and j<8){
+    Cyan();//przez kolejne 2 sekundy dioda RGB swieci w kolorze cyjan
+  }
+  if(j>=8 and j<10){
+    Blue();//przez kolejne 2 sekundy dioda RGB swieci w kolorze niebieskim
+  }
+  if(j>=10 and j<=12){
+    Magenta();//przez kolejne 2 sekundy dioda RGB swieci w kolorze magenta
+    
+  }
+  //gdy minie 12s (wykona sie caly cykl) j wraca do stanu poczatkowego
+  if(j==12){
+    j=0;}
+  i+=1;
+  j+=1;
+  delay(1000);//petla wykonuje sie co sekunde
+ 
+ 
+}
